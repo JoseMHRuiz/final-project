a0b37949cee9c7eb5fe30fbdb05c01295ab57c1c
Jose Manuel Henche Ruiz <josemanuel.hruiz@gmail.com>
2020-03-05 16:06:05 +0100

    First deploy

---
+++ back-end/public/precache-manifest.d21d4bb2bc2b924a8a051d3b1dee8c82.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0af9f8362967971d9bd779b2fe7a89ec",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2727b96c913275c2f0b1",
+    "url": "/static/css/main.91a5d3b5.chunk.css"
+  },
+  {
+    "revision": "4e87d0b15cdd6403bdc3",
+    "url": "/static/js/2.d0accbe5.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/static/js/2.d0accbe5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2727b96c913275c2f0b1",
+    "url": "/static/js/main.fe9f3002.chunk.js"
+  },
+  {
+    "revision": "691443989d219c585434",
+    "url": "/static/js/runtime-main.1eb18e34.js"
+  }
+]);
